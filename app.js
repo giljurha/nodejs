d4d682d93d3c7af96cad91ff6c749e11200dc043
gil <giljurha93@gmail.com>
2025-07-26 17:42:43 +0900

    push

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-//const cors = require('cors');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
-//app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', require('./routes/index'));
 
